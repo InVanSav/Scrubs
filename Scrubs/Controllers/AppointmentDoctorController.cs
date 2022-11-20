45fd996b39c3cf707a4ad86cea04c6d5be93dba2
InVanSav <savickijivan11@gmail.comsavickijivan11@gmail.com>
2022-11-20 21:31:22 +1000

    Migrations

--- Scrubs/Controllers/AppointmentDoctorController.cs
+++ Scrubs/Controllers/AppointmentDoctorController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace Scrubs.Controllers;
 
 using Domain.ViewModels.AppointmentDoctor;
-using Domain.ViewModels.User;
 using Microsoft.AspNetCore.Authorization;
 using Service.Interfaces;
 
--- Scrubs/Program.cs
+++ Scrubs/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Scrubs.DAL;
 using Scrubs.DAL.Interfaces;
 using Scrubs.DAL.Repositories;
 using Scrubs.Service.Implementations;
@@ -10,10 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+<<<<<<< HEAD
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connection));
 
+=======
+>>>>>>> test
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
 builder.Services.AddScoped<ISpecializationRepository, SpecializationRepository>();
